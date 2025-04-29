5718ca8fcba01ba58f84e45ef35cfd49f6fa0a1a
Armin Etemad <armin.ghorbanietemad@rub.de>
2025-04-29 16:05:02 +0200

    added license

--- src/multichss/MultiChSS_SpectrumPlotter.py
+++ src/multichss/MultiChSS_SpectrumPlotter.py
@@ -1,3 +1,10 @@
+# This file is part of SignalSnap (PyTorch): Signal Analysis In Python Made Easy
+# Copyright (c) 2024 and later, Armin Ghorbanietemed, Markus Sifft and Daniel Hägele.
+#
+# This software is provided under the terms of the 3-Clause BSD License.
+# For details, see the LICENSE file in the root of this repository or
+# https://opensource.org/licenses/BSD-3-Clause
+
 from .MultiChSS_SpectrumCalculator import SpectrumCalculator
 from .MultiChSS_SpectrumConfig import SpectrumConfig, DataImportConfig
 from .MultiChSS_CrossConfig import CrossConfig
